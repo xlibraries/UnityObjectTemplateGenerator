6dcbe26cf3ffdc51051cfd8806119206f8eab4ca
Xlib <aman9893089064@gmail.com>
2024-01-14 09:25:16 +0530

    Added the logic for creating multiple entries in a single Json file through Json Editor

---
+++ Assets/Scripts/JsonDataList.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+[System.Serializable]
+public class JsonDataList
+{
+    public List<JsonData> data;
+
+    public int Count { get; internal set; }
+}
--- Assets/Scripts/JsonEditor.cs
+++ Assets/Scripts/JsonEditor.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 
 public class JsonEditor : EditorWindow
 {
-    private JsonData newTemplate = new JsonData();
+    private List<JsonData> newTemplates = new List<JsonData>();
+    private string fileName = "JsonData";
+    private Vector2 scrollPosition;
 
     [MenuItem("Window/JSON Editor")]
     public static void ShowWindow()
@@ -17,27 +19,45 @@ public class JsonEditor : EditorWindow
     private void OnGUI()
     {
         GUILayout.Label("JSON Editor", EditorStyles.boldLabel);
+        fileName = EditorGUILayout.TextField("File Name", fileName);
 
-        // Create input fields for each property of JsonData
-        newTemplate.name = EditorGUILayout.TextField("Name", newTemplate.name);
-        newTemplate.position = EditorGUILayout.Vector3Field("Position", newTemplate.position);
-        newTemplate.rotation = EditorGUILayout.Vector3Field("Rotation", newTemplate.rotation);
-        newTemplate.scale = EditorGUILayout.Vector3Field("Scale", newTemplate.scale);
-        newTemplate.color = EditorGUILayout.ColorField("Color", newTemplate.color);
+        if (GUILayout.Button("New Template"))
+        {
+            newTemplates.Add(new JsonData());
+        }
+
+        scrollPosition = GUILayout.BeginScrollView(scrollPosition);
+        try
+        {
+            for (int i = 0; i < newTemplates.Count; i++)
+            {
+                JsonData template = newTemplates[i];
+                template.name = EditorGUILayout.TextField("Name", template.name);
+                template.position = EditorGUILayout.Vector3Field("Position", template.position);
+                template.rotation = EditorGUILayout.Vector3Field("Rotation", template.rotation);
+                template.scale = EditorGUILayout.Vector3Field("Scale", template.scale);
+                template.color = EditorGUILayout.ColorField("Color", template.color);
+
+                if (GUILayout.Button("Delete Template"))
+                {
+                    newTemplates.RemoveAt(i);
+                    return; // Exit the function to avoid an out-of-range exception
+                }
+            }
+        }
+        finally
+        {
+            GUILayout.EndScrollView();
+        }
 
         if (GUILayout.Button("Load JSON Data"))
         {
             LoadJsonData();
         }
 
-        if (GUILayout.Button("Create JSON Data"))
+        if (GUILayout.Button("Save JSON Data"))
         {
-            CreateJsonData();
-        }
-
-        if (GUILayout.Button("Update JSON Data"))
-        {
-            UpdateJsonData();
+            SaveJsonData();
         }
 
         if (GUILayout.Button("Delete JSON Data"))
@@ -53,26 +73,22 @@ public class JsonEditor : EditorWindow
         if (path.Length != 0)
         {
             string jsonString = File.ReadAllText(path);
-            newTemplate = JsonUtility.FromJson<JsonData>(jsonString);
+            JsonDataList jsonDataList = JsonUtility.FromJson<JsonDataList>(jsonString);
+            newTemplates = jsonDataList.data;
         }
     }
 
-    //Create JSON data
-    private void CreateJsonData()
+    //Save JSON data
+    private void SaveJsonData()
     {
-        string path = EditorUtility.SaveFilePanel("Create JSON File", "", newTemplate.name, "json");
+        string path = EditorUtility.SaveFilePanel("Save JSON File", "", fileName, "json");
         if (path.Length != 0)
         {
-            string jsonString = JsonUtility.ToJson(newTemplate);
+            Debug.Log("newTemplates count: " + newTemplates.Count); // Add this line
+            string jsonString = JsonUtility.ToJson(newTemplates, true);
+            Debug.Log("jsonString: " + jsonString); // Add this line
             File.WriteAllText(path, jsonString);
         }
-    }
-
-
-    //Edit JSON data
-    private void UpdateJsonData()
-    {
-        CreateJsonData();
     }
 
     //Delete JSON data
